72319e90bc021acf718206d3787f7560daee7c59
Payam Naderi <naderi.payam@gmail.com>
2016-10-31 10:59:44 +0330

    code cleanup

--- config/actions.conf.php
+++ config/actions.conf.php
@@ -4,8 +4,11 @@
  */
 return array(
     'services' => array(
-        'Module\OAuth2\Actions\Authorize' => array(':name' => 'Authorize'),
-        'Module\OAuth2\Actions\RespondToRequest' => array(':name' => 'RespondToRequest'),
-        'Module\OAuth2\Actions\RetrieveAuthenticatedUser' => array(':name' => 'RetrieveAuthenticatedUser'),
+        'Module\OAuth2\Actions\Authorize'
+           => array(\Poirot\Ioc\Container\BuildContainer::NAME => 'Authorize'),
+        'Module\OAuth2\Actions\RespondToRequest'
+           => array(\Poirot\Ioc\Container\BuildContainer::NAME => 'RespondToRequest'),
+        'Module\OAuth2\Actions\RetrieveAuthenticatedUser'
+           => array(\Poirot\Ioc\Container\BuildContainer::NAME => 'RetrieveAuthenticatedUser'),
     ),
 );
